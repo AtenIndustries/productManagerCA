79a55158e5c42faf0f58f86d816d8781bcfe371b
Abel Tenera <abel.tenera@massivestudios.io>
2026-09-05 14:47:01 +0100

    Add ProductsController Get and Create methods

--- ProductManager.API/Controllers/ProductsController.cs
+++ ProductManager.API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ProductManager.BAL.DTO;
+using ProductManager.BAL.Services.Interfaces;
 using ProductManager.DAL;
 using ProductManager.DAL.Models;
 
@@ -6,13 +8,26 @@ namespace ProductManager.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ProductsController : Controller
+public class ProductsController(IProductService productService) : Controller
 {
-    private readonly ProductManagerDBContext _dbContext;
+    private readonly IProductService _productService = productService;
 
-    public ProductsController(ProductManagerDBContext dBContext)
+    [HttpGet("{number}")]
+    [ProducesResponseType(typeof(ProductDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProductDTO>> Get(int number, CancellationToken ct)
     {
-        _dbContext=dBContext;
+        ProductDTO? product = await _productService.GetAsync(number, ct);
+        return product is null ? NotFound() : Ok(product);
     }
- 
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ProductDTO), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ProductDTO>> Create(ProductDTO product, CancellationToken ct)
+    {
+        int id = await _productService.CreateAsync(product, ct);
+        ProductDTO? created = await _productService.GetAsync(id, ct);
+        return CreatedAtAction(nameof(Create), new { id }, created);
+    }
 }
