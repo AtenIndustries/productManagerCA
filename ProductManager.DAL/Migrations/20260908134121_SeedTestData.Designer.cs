8622e7009b0a302702145ef562023d85ef65de6e
Abel Tenera <abel.tenera@massivestudios.io>
2026-09-08 14:42:43 +0100

    Add test data

--- ProductManager.DAL/Migrations/20260908134121_SeedTestData.Designer.cs
+++ ProductManager.DAL/Migrations/20260908134121_SeedTestData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManager.DAL;
 
@@ -11,9 +12,11 @@ using ProductManager.DAL;
 namespace ProductManager.DAL.Migrations
 {
     [DbContext(typeof(ProductManagerDBContext))]
-    partial class ProductManagerDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260908134121_SeedTestData")]
+    partial class SeedTestData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
